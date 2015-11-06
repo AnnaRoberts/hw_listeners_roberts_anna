fdc98b546daa3a2a38d42fff6c763703148c7a7c
Anna Roberts <annaroberts960608@gmail.com>
2015-11-05 23:49:01 -0500

    add comments in .js to explain first event listener

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+//This event listener will listen for a click, and then change the heading color to the list item color, yellow
+
 var Heading = document.getElementById( 'Heading' ) ;
 
 Heading.addEventListener('click', function() {
     Heading.className = 'yellow' ;
 });
+
+
