fb11e70b80b126d139d345fae527d385260f5a04
Anna Roberts <annaroberts960608@gmail.com>
2015-11-06 00:08:25 -0500

    add event listeners to change headings

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-//This event listener will listen for a click, and then change the heading color to the list item color, yellow
+//This event listener will listen for a click, and then change the heading
+//color to the list item color, yellow
 
 var Heading = document.getElementById( 'Heading' ) ;
 
@@ -6,4 +7,18 @@ Heading.addEventListener('click', function() {
     Heading.className = 'yellow' ;
 });
 
+//This event listener will listen for a click and then change the UL heading
+//that says "Three easy steps" to "I promise they're easy!"
 
+document.addEventListener('click', function(){
+    document.getElementById("demo").innerHTML = "I promise they are easy!";
+});
+
+
+//This event listener will listen for when you mouseover the bottom h2
+//'done' to change the heading from "That's It" to "see not so bad!"
+
+var done = document.getElementById( 'done')
+done.addEventListener('mouseover', function(){
+    document.getElementById("done").innerHTML = "See not so bad!";
+});
